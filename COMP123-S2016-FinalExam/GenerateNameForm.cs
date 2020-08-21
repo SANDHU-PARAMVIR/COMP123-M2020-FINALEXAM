a7657ab05c90e91e148f20b503a7807d4da20160
Param Vir Singh Sandhu <psandh63@my.centennialcollege.ca>
2020-08-21 11:08:25 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/GenerateNameForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public partial class GenerateNameForm : Form
+    {
+        public void GenerateNames()
+        {
+            var random = new Random();
+            int m_indexfirst = random.Next(0, FirstNameListBox.Items.Count);
+            firstNameTextBox.Text = FirstNameListBox.Items[m_indexfirst].ToString();
+
+            int m_indexlast = random.Next(0, lastNameListBox.Items.Count);
+            LastNameTextBox.Text = lastNameListBox.Items[m_indexlast].ToString();
+        }
+        public GenerateNameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateNameForm_Load(object sender, EventArgs e)
+        {
+            GenerateNames();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            
+            GenerateNames();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Program.character.FirstName = firstNameTextBox.Text;
+            Program.character.LastName = LastNameTextBox.Text;
+
+            AbilityGeneratorForm abilityGeneratorForm = new AbilityGeneratorForm();
+
+
+            abilityGeneratorForm.Show();
+
+            
+            Program.generatenameform.Hide();
+        }
+    }
+}
---
+++ COMP123-S2016-FinalExam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//  COMP 123_Programming 2_M2020_Final Exam
+//  Param Vir Singh Sandhu 
+//  ID: 301118847
+//
+//
+namespace COMP123_M2020_FinalExam
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+        public static AbilityGeneratorForm abilitygeneratorform;
+        private static AboutBox aboutbox;
+        public static FinalForm finalform;
+        public static GenerateNameForm generatenameform;
+        public static RaceAndClassForm raceandclassform;
+        public static SplashForm splashform;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            splashform = new SplashForm();
+            abilitygeneratorform = new AbilityGeneratorForm();
+            aboutbox = new AboutBox();
+            finalform = new FinalForm();
+            generatenameform = new GenerateNameForm();
+            raceandclassform = new RaceAndClassForm();
+
+            Application.Run(splashform);
+        }
+    }
+}
